c778898d2200090334e988eae0351af580c3912e
Tehreem Anwar <teanwar@knox.edu>
2020-05-28 07:03:52 -0500

    current version

---
+++ CS322/Assets/buttonscript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonscript : MonoBehaviour
+{
+    public void doquit(){
+    Debug.Log("Quit the Game");
+    Application.Quit();
+    }
+}
